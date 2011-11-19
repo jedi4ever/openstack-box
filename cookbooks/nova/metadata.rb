de3e61c77ed98cedcc49e8441e8e6f5dabc5d69f
Tim Dysinger <tim@dysinger.net>
2011-11-18 18:48:22 -0800

    Added nova metadata

---
+++ cookbooks/nova/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Tim Dysinger"
+maintainer_email "tim@dysinger.net"
+license          "Apache 2.0"
+description      "Installs/Configures OpenStack Nova"
+version          "0.1.0"
+supports         "ubuntu"
+depends          "apt"
