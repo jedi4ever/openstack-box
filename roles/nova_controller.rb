d9e32503339cf1ac3c730e0b2eaeb6de7e5afcc9
Tim Dysinger <tim@dysinger.net>
2011-11-20 20:28:28 +0100

    Add mysql to nova

--- roles/nova_controller.rb
+++ roles/nova_controller.rb
@@ -1,6 +1,7 @@
 name "nova_controller"
 run_list( "role[node]",
           "role[glance]",
+          "recipe[nova::mysql]",
           "recipe[nova::api]",
           "recipe[nova::network]",
           "recipe[nova::objectstore]",
