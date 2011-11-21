59262c39ec7023ddb93df0d4e4debd5b5a251d77
Tim Dysinger <tim@dysinger.net>
2011-11-21 14:04:35 +0100

    Remove wait (we aren't forking)

--- scripts/chef.sh
+++ scripts/chef.sh
@@ -6,7 +6,6 @@ if [ ! -f /etc/chef/client.pem ]; then
     for x in rabbitmq-server couchdb chef-solr chef-expander chef-server; do
         update-rc.d $x defaults
         invoke-rc.d $x start
-        wait
         if [ $x == rabbitmq-server ]; then
             rabbitmqctl add_vhost /chef
             rabbitmqctl add_user chef ub3rs3kr4t
