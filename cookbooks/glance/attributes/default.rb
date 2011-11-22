b26115742dc610bf225207914cf2eceb6941a0a3
Tim Dysinger <tim@dysinger.net>
2011-11-22 11:17:42 +0100

    Use attributes across the board

--- cookbooks/glance/attributes/default.rb
+++ cookbooks/glance/attributes/default.rb
@@ -2,3 +2,7 @@ default.glance.mysql.user = "glancedbadmin"
 set_unless.glance.mysql.password = "glancesecret"
 default.glance.mysql.database = "glance"
 set_unless.glance.mysql.host = "localhost"
+
+default.glance.sql_connection = "mysql://#{node.glance.mysql.user}:" +
+  "#{node.glance.mysql.password}@#{node.glance.mysql.host}/"  +
+  "#{node.glance.mysql.database}"
