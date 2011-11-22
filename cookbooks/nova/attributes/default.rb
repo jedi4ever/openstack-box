c07e90632961f79dbaaa322d26a8f455c8b502c3
Tim Dysinger <tim@dysinger.net>
2011-11-22 01:04:23 +0100

    Mysql database creation for nova working

---
+++ cookbooks/nova/attributes/default.rb
@@ -0,0 +1,3 @@
+default.nova.mysql.user = "novadbadmin"
+set_unless.nova.mysql.password = "novasecret"
+default.nova.mysql.database = "nova"
--- cookbooks/nova/recipes/mysql.rb
+++ cookbooks/nova/recipes/mysql.rb
@@ -1,1 +1,28 @@
 include_recipe "mysql::server"
+
+mysql_database node.nova.mysql.database do
+  connection({ :host => "localhost",
+               :username => "root",
+               :password => node.mysql.server_root_password })
+  action :create
+end
+
+execute "mysql-install-nova-privileges" do
+  command "/usr/bin/mysql -u root -p#{node.mysql.server_root_password} <\
+           /etc/mysql/nova-grants.sql"
+  action :nothing
+end
+
+template "/etc/mysql/nova-grants.sql" do
+  path "/etc/mysql/nova-grants.sql"
+  source "grants.sql.erb"
+  owner "root"
+  group "root"
+  mode "0600"
+  variables( :user     => node.nova.mysql.user,
+             :password => node.nova.mysql.password,
+             :database => node.nova.mysql.database )
+  notifies( :run,
+            resources(:execute => "mysql-install-nova-privileges"),
+            :immediately )
+end
