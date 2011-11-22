37033ac37b55401a3e15817fcb34024a43ebf5ae
Tim Dysinger <tim@dysinger.net>
2011-11-22 02:38:14 +0100

    Template the nova.conf file into place & sync the nova database

--- cookbooks/nova/recipes/default.rb
+++ cookbooks/nova/recipes/default.rb
@@ -4,3 +4,15 @@ package "nova-common"
 
 package "euca2ools"
 package "unzip"
+
+execute "nova-manage db sync" do
+  user "nova"
+  action :nothing
+end
+
+template "/etc/nova/nova.conf" do
+  owner "nova"
+  group "nova"
+  mode 0600
+  notifies :run, resources(:execute => "nova-manage db sync"), :immediately
+end
