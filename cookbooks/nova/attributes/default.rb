a2f21b309a59fead046b1f42a15d1918b7ad5a4d
Tim Dysinger <tim@dysinger.net>
2011-11-22 02:38:53 +0100

    Sorting attributes

--- cookbooks/nova/attributes/default.rb
+++ cookbooks/nova/attributes/default.rb
@@ -3,6 +3,6 @@ set_unless.nova.mysql.password = "novasecret"
 default.nova.mysql.database = "nova"
 set_unless.nova.mysql.host = "localhost"
 
-set_unless.nova.rabbitmq.password = "secure_password"
 default.nova.rabbitmq.user = "nova"
+set_unless.nova.rabbitmq.password = "secure_password"
 default.nova.rabbitmq.vhost = "/nova"
